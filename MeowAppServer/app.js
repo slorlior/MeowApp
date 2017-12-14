a33e70dac62a8fec4dd87141690befab965553a4
Lior Slor <slor.lior@gmail.com>
2017-12-14 13:44:34 +0200

    server can listen to default process port now

--- MeowAppServer/app.js
+++ MeowAppServer/app.js
@@ -33,6 +33,6 @@ app.use(function(err, req, res, next) {
     });
   });
 
-app.listen(3000, function () {
-  console.log('MeowApp listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('MeowApp is up!');
 });
